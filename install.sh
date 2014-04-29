a4b7a34a233ae34c36b9bd5438204916428232f9
Victor Cheng <victor.cheng@paperg.com>
2014-04-29 13:59:28 -0700

    Install unzip if necessary

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 CONSUL_ZIP_URL="https://dl.bintray.com/mitchellh/consul/0.1.0_linux_amd64.zip"
 
+
+test -x /usr/bin/unzip || sudo apt-get install unzip
+
 cd $SCRIPT_DIR
 
 test -f consul.zip || wget --output-document=consul.zip $CONSUL_ZIP_URL
